ac06e99a0920bd784521984c41f0bb97551dfbd7
Vladimir Cibizov <vladimir.tsibizow@gmail.com>
2025-07-16 22:18:43 +0300

    Update radial-orbital-timeline.tsx

--- apps/frontend/src/components/HeroBlock/radial-orbital-timeline.tsx
+++ apps/frontend/src/components/HeroBlock/radial-orbital-timeline.tsx
@@ -272,7 +272,6 @@ export default function RadialOrbitalTimeline({
                           {item.date}
                         </span>
                       </div> */}
-                      </div>
                       <CardTitle className="text-sm mt-2">
                         {item.title}
                       </CardTitle>
